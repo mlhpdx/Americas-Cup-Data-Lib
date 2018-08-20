b242b8a7aee85afdf58a845b3d7b0df0df659ecd
Lee Harding <lharding@elevensoftware.com>
2018-08-19 20:06:19 -0700

    Fix leaking XmlSerializers.

--- AmericasCup.Feed/FeedEvents.cs
+++ AmericasCup.Feed/FeedEvents.cs
@@ -8,15 +8,15 @@ using System.Xml.Serialization;
 
 namespace AmericasCup.Feed
 {
-    public class FeedEvents
-    {
-        public FeedEvents() { InitializeMessageHandlers(); }
+   public class FeedEvents
+   {
+      public FeedEvents() { InitializeMessageHandlers(); }
 
-        public Dictionary<MessageTypeEnum, Action<byte[], byte[]>> MessageTypeHandlers;
+      public Dictionary<MessageTypeEnum, Action<byte[], byte[]>> MessageTypeHandlers;
 
-        public void InitializeMessageHandlers()
-        {
-            MessageTypeHandlers = new Dictionary<MessageTypeEnum, Action<byte[], byte[]>>()
+      public void InitializeMessageHandlers()
+      {
+         MessageTypeHandlers = new Dictionary<MessageTypeEnum, Action<byte[], byte[]>>()
             {
                 { MessageTypeEnum.Heartbeat, (header, body) => { if (OnHeartbeat != null) OnHeartbeat(Heartbeat.Read(body)); } },
                 { MessageTypeEnum.RaceStatus, (header, body) => { if (OnRaceStatus != null) OnRaceStatus(RaceStatus.Read(body)); } },
@@ -31,71 +31,73 @@ namespace AmericasCup.Feed
                 { MessageTypeEnum.CourseWind, (header,body) => { if (OnCourseWind != null) OnCourseWind(CourseWind.Read(body)); } },
                 { MessageTypeEnum.AverageWind, (header,body) => { if (OnAverageWind != null) OnAverageWind(AverageWind.Read(body)); } }
             };
-        }
+      }
 
-        public void MessageHandler(byte[] header, byte[] body, byte[] crc)
-        {
-            var type = (MessageTypeEnum)header[2];
+      public void MessageHandler(byte[] header, byte[] body, byte[] crc)
+      {
+         var type = (MessageTypeEnum)header[2];
 
-            if (MessageTypeHandlers.ContainsKey(type))
-                MessageTypeHandlers[type](header, body);
-            else if (OnUnsupportedMessage != null)
-            {
-                var msg = new Message();
-                MessageHeader.Read(header, 0, msg.Header);
-                Message.Read(body, 0, msg);
-                msg.Crc = BitConverter.ToUInt32(crc, 0);
+         if (MessageTypeHandlers.ContainsKey(type))
+            MessageTypeHandlers[type](header, body);
+         else if (OnUnsupportedMessage != null)
+         {
+            var msg = new Message();
+            MessageHeader.Read(header, 0, msg.Header);
+            Message.Read(body, 0, msg);
+            msg.Crc = BitConverter.ToUInt32(crc, 0);
 
-                OnUnsupportedMessage(msg);
-            }
-        }
+            OnUnsupportedMessage(msg);
+         }
+      }
 
-        void HandleXmlMessage<T>(string text, Action<T> action) where T : class
-        {
-            if (action != null)
-            {
-                var s = new XmlSerializer(typeof(T));
-                var t = s.Deserialize(new System.IO.StringReader(text)) as T;
-                action(t);
-            }
-        }
+`      static Dictionary<Type, XmlSerializer> _serializers = new Dictionary<Type, XmlSerializer>();
+      void HandleXmlMessage<T>(string text, Action<T> action) where T : class
+      {
+         if (action != null)
+         {
+            var t = typeof(T);
+            var s = _serializers.ContainsKey(t) ? _serializers[t] : _serializers[t] = new XmlSerializer(t);
+            var xml = s.Deserialize(new System.IO.StringReader(text)) as T;
+            action(xml);
+         }
+      }
 
-        void HandleXmlMessage(XmlMessage m)
-        {
-            switch (m.SubType)
-            {
-                case XmlMessageSubTypeEnum.Regatta:
-                    HandleXmlMessage(m.Text, OnRegattaConfig);
-                    break;
-                case XmlMessageSubTypeEnum.Race:
-                    HandleXmlMessage(m.Text, OnRaceConfig);
-                    break;
-                case XmlMessageSubTypeEnum.Boat:
-                    HandleXmlMessage(m.Text, OnBoatConfig);
-                    break;
-                default:
-                    if (OnUnsupportedXmlMessage != null) OnUnsupportedXmlMessage(m);
-                    break;
-            }
-        }
+      void HandleXmlMessage(XmlMessage m)
+      {
+         switch (m.SubType)
+         {
+            case XmlMessageSubTypeEnum.Regatta:
+               HandleXmlMessage(m.Text, OnRegattaConfig);
+               break;
+            case XmlMessageSubTypeEnum.Race:
+               HandleXmlMessage(m.Text, OnRaceConfig);
+               break;
+            case XmlMessageSubTypeEnum.Boat:
+               HandleXmlMessage(m.Text, OnBoatConfig);
+               break;
+            default:
+               if (OnUnsupportedXmlMessage != null) OnUnsupportedXmlMessage(m);
+               break;
+         }
+      }
 
-        public event Action<RegattaConfig> OnRegattaConfig;
-        public event Action<RaceConfig> OnRaceConfig;
-        public event Action<BoatConfig> OnBoatConfig;
+      public event Action<RegattaConfig> OnRegattaConfig;
+      public event Action<RaceConfig> OnRaceConfig;
+      public event Action<BoatConfig> OnBoatConfig;
 
-        public event Action<Heartbeat> OnHeartbeat;
-        public event Action<RaceStatus> OnRaceStatus;
-        public event Action<DisplayTextMessage> OnDisplayTextMessage;
-        public event Action<RaceStartStatus> OnRaceStartStatus;
-        public event Action<YachtEventCode> OnYachtEventCode;
-        public event Action<YachtActionCode> OnYachtActionCode;
-        public event Action<ChatterText> OnChatterText;
-        public event Action<BoatLocation> OnBoatLocation;
-        public event Action<MarkRounding> OnMarkRounding;
-        public event Action<CourseWind> OnCourseWind;
-        public event Action<AverageWind> OnAverageWind;
+      public event Action<Heartbeat> OnHeartbeat;
+      public event Action<RaceStatus> OnRaceStatus;
+      public event Action<DisplayTextMessage> OnDisplayTextMessage;
+      public event Action<RaceStartStatus> OnRaceStartStatus;
+      public event Action<YachtEventCode> OnYachtEventCode;
+      public event Action<YachtActionCode> OnYachtActionCode;
+      public event Action<ChatterText> OnChatterText;
+      public event Action<BoatLocation> OnBoatLocation;
+      public event Action<MarkRounding> OnMarkRounding;
+      public event Action<CourseWind> OnCourseWind;
+      public event Action<AverageWind> OnAverageWind;
 
-        public event Action<Message> OnUnsupportedMessage;
-        public event Action<XmlMessage> OnUnsupportedXmlMessage;
-    }
+      public event Action<Message> OnUnsupportedMessage;
+      public event Action<XmlMessage> OnUnsupportedXmlMessage;
+   }
 }
